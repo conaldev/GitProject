52d1ee65f3b0cbcb35da1c0ee06c48f0609737c8
conaldev <talaaidoaibiet@gmail.com>
2020-06-14 18:13:34 +0700

    bt

---
+++ Week-3-module-1/Day14/th-tim-gia-tri-nho-nhat-mang.js
@@ -0,0 +1,14 @@
+function minArray(arr) {
+    if(arr.length == 0)
+        return -1;
+    let min = arr[0];
+
+    for(let i = 1; i < arr.length; i++){
+        if(arr[i] < min){
+            min = arr[i];
+        }
+    }
+    return min;
+}
+let arr2 = [7, 12, 6, 9, 20, 56, 89];
+console.log(minArray(arr2));
